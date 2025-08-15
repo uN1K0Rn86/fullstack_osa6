99d3cdc68207a4e6b9c568343f3c4d671a30c33b
Sami Heinimo <sami.heinimo@helsinki.fi>
2025-08-15 10:38:13 +0300

    Part 6 Exercise 16

--- redux-anecdotes/src/reducers/anecdoteReducer.jsx
+++ redux-anecdotes/src/reducers/anecdoteReducer.jsx
@@ -1,14 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit"
-
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
+import anecdoteService from '../services/anecdotes'
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
@@ -36,5 +27,12 @@ const anecdoteSlice = createSlice({
 })
 
 export const { createAnecdote, addVote, setAnecdotes } = anecdoteSlice.actions
+
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch(setAnecdotes(anecdotes))
+  }
+}
 
 export default anecdoteSlice.reducer
