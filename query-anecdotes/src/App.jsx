d2c22f7d120aeb2b55334355b6ee029940b481ca
Sami Heinimo <sami.heinimo@helsinki.fi>
2025-08-15 13:28:03 +0300

    Part 6 Exercise 20

---
+++ query-anecdotes/src/App.jsx
@@ -0,0 +1,52 @@
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query"
+import { getAnecdotes } from "./requests"
+
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+
+const App = () => {
+  const queryClient = useQueryClient()
+
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes
+  })
+  console.log(JSON.parse(JSON.stringify(result)))
+
+  if ( result.isLoading ) {
+    return <div>loading data...</div>
+  }
+
+  if ( result. isError ) {
+    return <div>anecdote service is currently unavailable</div>
+  }
+
+  const handleVote = (anecdote) => {
+    console.log('vote')
+  }
+
+  const anecdotes = result.data
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+    
+      <Notification />
+      <AnecdoteForm />
+    
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
---
+++ query-anecdotes/src/requests.js
@@ -0,0 +1,14 @@
+import axios from "axios"
+
+const baseUrl = 'http://localhost:3001/anecdotes'
+
+export const getAnecdotes = () => {
+  return axios.get(baseUrl).then(res => res.data)
+}
+
+export const createAnecdote = newAnecdote => {
+  return axios.post(baseUrl, newAnecdote).then(res => res.data)
+}
+
+export const updateNote = updatedAnecdote =>
+  axios.put(`${baseUrl}/${updatedAnecdote.id}`, updatedAnecdote).then(res => res.data)
